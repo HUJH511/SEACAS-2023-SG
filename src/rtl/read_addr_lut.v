40c1ac51d6de847b75537798975c32b99090b0f9
Andrés Brito <a.santiago.brito.c@gmail.com>
2023-09-18 23:03:00 +0800

    Update read_addr_lut.v
    Back to the original radix-2

--- src/rtl/read_addr_lut.v
+++ src/rtl/read_addr_lut.v
@@ -4,19 +4,12 @@
 // Module Name: read_addr_lut
 // Project Name: Fast Fourier Transform (16-point)
 ///////////////////////////////////////////////////////////////////////////
-
 module read_addr_lut (
-   input wire stage,                // Only 2 stages (1 bit)
-   input wire [1:0] butterfly,      // 4 butterflies (2 bits)
-   
-   output reg [3:0] A_addr,         // radix-4
+   input wire [1:0] stage,
+   input wire [2:0] butterfly,
+   output reg [3:0] A_addr,
    output reg [3:0] B_addr,
-   output reg [3:0] C_addr,
-   output reg [3:0] D_addr,
-
-   output reg [3:0] W_addr_b,       // To reach W9 (4 bits)
-   output reg [3:0] W_addr_c,
-   output reg [3:0] W_addr_d
+   output reg [2:0] W_addr
 );
 
    always @(*) begin
@@ -28,46 +21,50 @@ module read_addr_lut (
 
                0: begin
                   A_addr = 0;
-                  B_addr = 4;
-                  C_addr = 8;
-                  D_addr = 12;
-
-                  W_addr_b = 0;
-                  W_addr_c = 0;
-                  W_addr_d = 0;
+                  B_addr = 8;
+                  W_addr = 0;
                end
 
                1: begin
-                  A_addr = 1;
-                  B_addr = 5;
-                  C_addr = 9;
-                  D_addr = 13;
-
-                  W_addr_b = 0;
-                  W_addr_c = 0;
-                  W_addr_d = 0;
+                  A_addr = 4;
+                  B_addr = 12;
+                  W_addr = 0;
                end
 
                2: begin
                   A_addr = 2;
-                  B_addr = 6;
-                  C_addr = 10;
-                  D_addr = 14;
-
-                  W_addr_b = 0;
-                  W_addr_c = 0;
-                  W_addr_d = 0;
+                  B_addr = 10;
+                  W_addr = 0;
                end
 
                3: begin
-                  A_addr = 3;
-                  B_addr = 7;
-                  C_addr = 11;
-                  D_addr = 15;
+                  A_addr = 6;
+                  B_addr = 14;
+                  W_addr = 0;
+               end
 
-                  W_addr_b = 0;
-                  W_addr_c = 0;
-                  W_addr_d = 0;
+               4: begin
+                  A_addr = 1;
+                  B_addr = 9;
+                  W_addr = 0;
+               end
+
+               5: begin
+                  A_addr = 5;
+                  B_addr = 13;
+                  W_addr = 0;
+               end
+
+               6: begin
+                  A_addr = 3;
+                  B_addr = 11;
+                  W_addr = 0;
+               end
+
+               7: begin
+                  A_addr = 7;
+                  B_addr = 15;
+                  W_addr = 0;
                end
 
             endcase
@@ -78,50 +75,162 @@ module read_addr_lut (
 
                0: begin
                   A_addr = 0;
-                  B_addr = 1;
-                  C_addr = 2;
-                  D_addr = 3;
-
-                  W_addr_b = 0;
-                  W_addr_c = 0;
-                  W_addr_d = 0;
+                  B_addr = 4;
+                  W_addr = 0;
                end
 
                1: begin
-                  A_addr = 4;
-                  B_addr = 5;
-                  C_addr = 6;
-                  D_addr = 7;
-
-                  W_addr_b = 1;
-                  W_addr_c = 2;
-                  W_addr_d = 3;
+                  A_addr = 8;
+                  B_addr = 12;
+                  W_addr = 4;
                end
 
                2: begin
+                  A_addr = 2;
+                  B_addr = 6;
+                  W_addr = 0;
+               end
+
+               3: begin
+                  A_addr = 10;
+                  B_addr = 14;
+                  W_addr = 4;
+               end
+
+               4: begin
+                  A_addr = 1;
+                  B_addr = 5;
+                  W_addr = 0;
+               end
+
+               5: begin
+                  A_addr = 9;
+                  B_addr = 13;
+                  W_addr = 4;
+               end
+
+               6: begin
+                  A_addr = 3;
+                  B_addr = 7;
+                  W_addr = 0;
+               end
+
+               7: begin
+                  A_addr = 11;
+                  B_addr = 15;
+                  W_addr = 4;
+               end
+
+            endcase
+         end // case: 1
+
+         2: begin
+            case (butterfly)
+
+               0: begin
+                  A_addr = 0;
+                  B_addr = 2;
+                  W_addr = 0;
+               end
+
+               1: begin
+                  A_addr = 8;
+                  B_addr = 10;
+                  W_addr = 2;
+               end
+
+               2: begin
+                  A_addr = 4;
+                  B_addr = 6;
+                  W_addr = 4;
+               end
+
+               3: begin
+                  A_addr = 12;
+                  B_addr = 14;
+                  W_addr = 6;
+               end
+
+               4: begin
+                  A_addr = 1;
+                  B_addr = 3;
+                  W_addr = 0;
+               end
+
+               5: begin
+                  A_addr = 9;
+                  B_addr = 11;
+                  W_addr = 2;
+               end
+
+               6: begin
+                  A_addr = 5;
+                  B_addr = 7;
+                  W_addr = 4;
+               end
+
+               7: begin
+                  A_addr = 13;
+                  B_addr = 15;
+                  W_addr = 6;
+               end
+
+            endcase
+         end // case: 2
+
+         3: begin
+            case (butterfly)
+
+               0: begin
+                  A_addr = 0;
+                  B_addr = 1;
+                  W_addr = 0;
+               end
+
+               1: begin
                   A_addr = 8;
                   B_addr = 9;
-                  C_addr = 10;
-                  D_addr = 11;
+                  W_addr = 1;
+               end
 
-                  W_addr_b = 2;
-                  W_addr_c = 4;
-                  W_addr_d = 6;
+               2: begin
+                  A_addr = 4;
+                  B_addr = 5;
+                  W_addr = 2;
                end
 
                3: begin
                   A_addr = 12;
                   B_addr = 13;
-                  C_addr = 14;
-                  D_addr = 15;
+                  W_addr = 3;
+               end
 
-                  W_addr_b = 3;
-                  W_addr_c = 6;
-                  W_addr_d = 9;
+               4: begin
+                  A_addr = 2;
+                  B_addr = 3;
+                  W_addr = 4;
+               end
+
+               5: begin
+                  A_addr = 10;
+                  B_addr = 11;
+                  W_addr = 5;
+               end
+
+               6: begin
+                  A_addr = 6;
+                  B_addr = 7;
+                  W_addr = 6;
+               end
+
+               7: begin
+                  A_addr = 14;
+                  B_addr = 15;
+                  W_addr = 7;
                end
 
             endcase
-         end // case: 1
+         end // case: 3
 
       endcase
 
